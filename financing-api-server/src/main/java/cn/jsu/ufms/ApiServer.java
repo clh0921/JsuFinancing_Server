b7c726b0e182385517b3b5806df6a1104212534d
fsdcyr <fsdcyr@live.com>
2017-04-12 00:35:06 +0800

    build env

---
+++ financing-api-server/src/main/java/cn/jsu/ufms/ApiServer.java
@@ -0,0 +1,22 @@
+package cn.jsu.ufms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class ApiServer {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    public static void main( String[] args ) {
+        SpringApplication.run(ApiServer.class);
+    }
+
+}
